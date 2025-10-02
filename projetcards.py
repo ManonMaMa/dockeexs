7859e450c8a1e2e57cc4b891defd2d16144addda
ManonMaMa <infini030303@gmail.com>
2025-10-02 14:43:02 +0200

    vers new_personnage

--- projetcards.py
+++ projetcards.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/new_personnage")
+def new_personnage():
+    return render_template('new_personnage.html')
+
 if __name__ == '__main__':
     app.run(debug = True)
 
