bb64392f8f5a75a386811e396b565f3c2b15a413
ManonMaMa <infini030303@gmail.com>
2025-10-03 12:09:19 +0200

    docker

--- projetcards.py
+++ projetcards.py
@@ -43,8 +43,5 @@ def upload_file():
 
 # mode debug et hot reload actif
 if __name__ == '__main__':
-    app.run(debug = True)
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
