246352c6ff584a3f10c25014e02b1fa9cf0feb0d
Pierre Bullot <pierre-bullot@hotmail.fr>
2025-10-03 09:42:51 +0200

    Tous les fichiers dans le dossier 'static' sont affichés en temps qu'image sur la page 'galerie'

--- projetcards.py
+++ projetcards.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask import render_template
+import os
 
 app = Flask(__name__)
+
+IMG_FOLDER = os.path.join("static")
+app.config["UPLOAD_FOLDER"] = IMG_FOLDER
 
 @app.route("/")
 def index():
@@ -13,7 +17,9 @@ def new_personnage():
 
 @app.route("/galerie")
 def galerie():
-    return render_template('galerie.html')
+    test_list = os.listdir("static")
+    test_list = ["/" + image for image in test_list]
+    return render_template('galerie.html', list_test=test_list)
 
 if __name__ == '__main__':
     app.run(debug = True)
