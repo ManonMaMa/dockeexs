6a413b4d0cc0c68c965c0353e80e122c84c6d0a7
Muriel Bullot <murielbullot@hotmail.fr>
2025-10-06 14:18:46 +0200

    ajout des explications

--- projetcards.py
+++ projetcards.py
@@ -1,6 +1,5 @@
 import os
-from flask import Flask, request
-from flask import render_template
+from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 
 # Lors du développement d'une app Flask, mettre :
@@ -10,44 +9,52 @@ from werkzeug.utils import secure_filename
 # création de l'application Flask ( on indique que ce fichier est le fichier principal)
 app = Flask(__name__)       # __name__ : variable spéciale de python contenant le nom de ce fichier
 
+# Définition de la route principale :  http://127.0.0.1:5000/
+# methode 1 : affichage d'images sans utiliser de variable
+@app.route("/")                             
+def index():
+    return render_template('index.html')        # fonction de Flask qui va chercher index.html dans le dossier templates/
+
 # gestion des fichiers uploadés (images envoyées par un formulaire)
-IMG_FOLDER = os.path.join("static", "uploaded_images")      # écriture d'un chemin lisible par tous (windows, linux, mac)
+IMG_FOLDER = os.path.join('static', 'uploaded_images')      # écriture d'un chemin lisible par tous (windows, linux, mac)
 app.config["UPLOAD_FOLDER"] = IMG_FOLDER                    # indique à Flask le chemin pour faire les sauvegardes
 
 # protection : liste d’extensions de fichiers autorisées
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
-# Définition de la route principale :  http://127.0.0.1:5000/
-@app.route("/")                             
-def index():
-    return render_template('index.html')        # fonction de Flask qui va chercher index.html dans le dossier templates/
-
 # Définition d'une route :  http://127.0.0.1:5000/galerie
 @app.route("/galerie")
 def galerie():
-    image_list = os.listdir("static/uploaded_images")
+    image_list = os.listdir(IMG_FOLDER)
     image_list = ["uploaded_images/" + image for image in image_list]
     return render_template('galerie.html', liste_images=image_list)
 
+# vérifier que le nom du fichier à une extension qui se trouve dans ALLOWED_EXTENSIONS
 def allowed_file(filename):
+    # divise le nom du fichier en liste en séparant par le caractère "point"
+    # [1] : prend la deuxième partie
+    # transforme l’extension en minuscules
+    # return true si l'extension se trouve dans ALLOWED_EXTENSIONS, false sinon
     return filename.split('.')[1].lower() in ALLOWED_EXTENSIONS
 
 # Définition d'une route :  http://127.0.0.1:5000/new_personnage
 @app.route('/new_personnage', methods=['GET', 'POST'])
 def upload_file():
+    # vérifier que l'on récupère les données d'un formulaire
     if request.method == 'POST':
-        # check if the post request has the file part
+        # si request.files est vide car 'file' n'a pas été transmis du tout
         if 'file' not in request.files:
             return render_template('new_personnage.html')
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
+        file = request.files['file']        # variable file qui récupère le fichier 'file' envoyé par l'utilisateur
+        # si le nom du fichier est vide 
         if file.filename == '':
             return render_template('new_personnage.html')
+        # si l'extension est correcte
         elif allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filename = secure_filename(file.filename)                           # Nettoie le nom du fichier pour enlever les caractères dangereux ou les espaces.
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))      # sauvegarde le fichier à l'endroit indiqué
             return render_template('new_personnage.html')
+    # s'il ne s'agit pas d'un fichier, redémarrer la page
     return render_template('new_personnage.html')
 
 # Lancement du serveur : mode debug et hot reload actif
