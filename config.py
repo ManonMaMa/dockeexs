4e8db3451d11b3d3e3fe1b3c5503de69d529eb0f
Pierre Bullot <pierre-bullot@hotmail.fr>
2025-10-07 16:30:36 +0200

    Clef OpenRouter initiale réintroduite, fonction save_file crée.

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 # Contient les constantes utilisées dans le programme.
 
 # Clef pour s'identifier auprès du LLM.
-OPENROUTER_KEY = "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" # "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"
+OPENROUTER_KEY = "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"
+OPENROUTER_KEY_2 = "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"
 
 # Folder containing the images uploaded by users.
 IMG_FOLDER = '/app/static/uploaded_images'
